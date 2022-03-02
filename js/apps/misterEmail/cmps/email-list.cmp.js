00e12a175549d710b7e06909d985459aef5feedd
Elana Hecht <elanabrick@gmail.com>
2022-03-02 18:20:22 +0200

    commit-after break

--- js/apps/misterEmail/cmps/email-list.cmp.js
+++ js/apps/misterEmail/cmps/email-list.cmp.js
@@ -4,6 +4,7 @@ export default {
    props: ['emails'],
    template: `
             <section class="email-folder-list">
+               <h1 v-if="!emails">You don't have any emails...</h1>
                <ul>
                   <li v-for="email in emails" :key="email.id">
                <email-preview :email="email"/>
