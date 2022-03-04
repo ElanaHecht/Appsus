77d22836574b24559ce459bc2d1cabd544dfafdb
Igor <izingor@gmail.com>
2022-03-05 00:39:44 +0200

    added columns for notes

--- js/apps/missKeep/components/pinned-notes.cmp.js
+++ js/apps/missKeep/components/pinned-notes.cmp.js
@@ -4,7 +4,7 @@ import notePreview from './note-preview.cmp.js';
 export default{
     props:['notes'],
     template:`
-        <section class = 'notes-list main-layout flex'>
+        <section class = 'pinned-list main-layout'>
               <note-preview v-for="note in notes" :key="note.id" :note = "note"></note-preview>
         </section>  
     `,
